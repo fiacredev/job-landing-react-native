37c6a9306073c604ce2a3e6ed0b26f00a759017d
fiacredev <fiacredeveloper@gmail.com>
2026-03-10 21:22:42 +0200

    over write files on github because of i wanna push the one with modified changes about handling Server for every API Request

--- app/utils/socket.ts
+++
@@ -1,9 +0,0 @@
-import { io } from "socket.io-client";
-
-let socket: any;
-
-export const connectSocket = () => {
-  socket = io("https://curb-side-backend.onrender.com");
-};
-
-export const getSocket = () => socket;
